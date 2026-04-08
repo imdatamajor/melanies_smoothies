a3298724b34c07c34114e4bff3433643b6e214c5
imdatamajor <ihab.marsou@data-major.com>
2026-04-08 11:28:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,7 @@ smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/wa
 #st.text(smoothiefroot_response.json())
 
 
-sf_df = st.dataframe(data=smoothiefruit_response.json(), use_container_width=True)
-
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
